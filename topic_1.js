9256378dcdf4a2cc369242d2a9b30f4bc5ba9a7d
salixes <salizpersonaluse@gmail.com>
2024-10-03 13:38:44 +0800

    OOP

---
+++ topic_1.js
@@ -0,0 +1,11 @@
+//Input
+const person1 ={
+    name:"Saliz II Pag-oy",
+    age:18,
+    occupation:"TAMBAY"
+}
+
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Occupation: "+person1.occupation);
